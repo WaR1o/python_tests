c99f646303ec756954203269a84a9800ab03e266
WaR1o <shok163@gmail.com>
2016-10-05 17:00:10 +0400

    Оптимизированы переходы на страницу групп. Убрана задержка драйвера

--- fixture/group.py
+++ fixture/group.py
@@ -5,6 +5,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
+        if wd.current_url.endswith("/groups.php") and len(wd.find_elements_by_name("new")) > 0:
+            return
         wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
@@ -57,3 +59,8 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
